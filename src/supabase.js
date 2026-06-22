bf975a942f1695ec544a8a6cf97019e5aa23a6d9
miyamononaoki-arch <miyamononaoki@gmail.com>
2026-06-22 22:19:50 +0900

    initial commit

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = 'https://mtpbskfnkgjqltlspifm.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_2rDHbZocC1uH9XihYYxzAA_rLBMg28w';
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
